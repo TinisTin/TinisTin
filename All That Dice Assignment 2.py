5e0d1a0c00da057822edad1026b9ab67f6c9d6fc
TinisTin <105897244+TinisTin@users.noreply.github.com>
2022-11-13 18:07:01 +1030

    Testing

---
+++ All That Dice Assignment 2.py
@@ -0,0 +1,11 @@
+#
+# File: All-That-Dice.py
+# Description: Blah Blah Blah
+# Author: Trong Le
+# Student ID: 110373303
+# Email ID: leytt028
+# This is my own work as defined by
+# the University's Academic Misconduct Policy.
+#
+
+#CHeck 
